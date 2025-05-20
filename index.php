151beb656fceb3e625893788dce09366f6185cb8
Asmul Acer <asep_ulum@yahoo.com>
2025-05-20 09:28:57 +0700

    komen koneksi cek

--- index.php
+++ index.php
@@ -7,9 +7,9 @@ $dbname = "regina";
 // Create connection
 $conn = new mysqli($servername, $username, $password, $dbname);
 // Check connection
-if ($conn->connect_error) {
-  die("Connection failed: " . $conn->connect_error);
-}
+// if ($conn->connect_error) {
+//   die("Connection failed: " . $conn->connect_error);
+// }
 
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
   $nama = $_POST['nama'];
